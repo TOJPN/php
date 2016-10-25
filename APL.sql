d6d902a6fe25c9a500f0ed0793eecc68389f407d
TOJPN <t.okabe.ous@gmail.com>
2016-10-26 02:40:31 +0900

    Add files via upload

---
+++ APL.sql
@@ -0,0 +1,48 @@
+INSERT INTO feelings VALUES(NULL,'ルンルン');
+/
+INSERT INTO feelings VALUES(NULL,'ノリノリ');
+/
+INSERT INTO feelings VALUES(NULL,'ホノボノ');
+/
+INSERT INTO feelings VALUES(NULL,'ラブラブ');
+/
+INSERT INTO feelings VALUES(NULL,'ヘロヘロ');
+/
+INSERT INTO feelings VALUES(NULL,'ガックリ');
+/
+INSERT INTO artists VALUES(NULL,'ソフトズ');
+/
+INSERT INTO artists VALUES(NULL,'丸見栄子');
+/
+INSERT INTO artists VALUES(NULL,'コナチーズ');
+/
+INSERT INTO artists VALUES(NULL,'上部安埔里');
+/
+INSERT INTO artists VALUES(NULL,'森見タマエ');
+/
+INSERT INTO artists VALUES(NULL,'ピーエッチピー');
+/
+INSERT INTO tunes VALUES(NULL,'春だよね',1,1,'春が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'夏だよね',1,2,'夏が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'秋だよね',1,3,'秋が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'冬だよね',1,4,'冬が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'朝が来た',3,5,'朝が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'昼だよ',3,6,'昼が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'もう夜',4,1,'これを聴くと眠くなります',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'あけがた',4,2,'朝が来ると聴きたくなる曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'楽しいな',5,3,'楽しいときに聴きたい曲です',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'うれしいな',5,4,'これを聴くとうれしくなります',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'迷っちゃう',6,5,'もう迷いたくないです',NOW());
+/
+INSERT INTO tunes VALUES(NULL,'もう食べられない',6,6,'満腹状態になると聴きたくなります',NOW());
+/
